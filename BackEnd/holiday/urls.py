a340ac01aacf1d2166fa8cf14eb56570d26e6341
parham <p.zilouchian@gmail.com>
2018-05-10 16:46:30 +0430

    Some Restaurants Bugs Fix... Carts coming Soon!!!

--- BackEnd/holiday/urls.py
+++ BackEnd/holiday/urls.py
@@ -16,19 +16,20 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 from attractions.views import HomeView, ContactView, AboutView
+# from restaurants.views import RestaurantCreateView, RestaurantListView
 
 urlpatterns = [
-
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
     # url(r'^restaurants/create/$', RestaurantCreateView.as_view(), name='restaurants-create'),
-    # url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
+    # url(r'^restaurants/$', RestaurantListView.as_view(), name='restaurants-list'),
     # url(r'^restaurants/(?P<pk>\w+)/$', RestaurantDetailView.as_view(), name='restaurant-detail'),
-    url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
-    url(r'^account/', include('accounts.urls', namespace='account')),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^accounts/', include('accounts.urls', namespace='account')),
     url(r'^tours/', include('tours.urls', namespace='tours')),
-
 ]
--- BackEnd/restaurants/forms.py
+++ BackEnd/restaurants/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import RestaurantLocation, Item
-from .validators import validate_category
 
 
 class RestaurantCreateForm(forms.Form):
@@ -16,9 +15,6 @@ class RestaurantCreateForm(forms.Form):
 
 
 class RestaurantLocationCreateForm(forms.ModelForm):
-    # email = forms.EmailField()
-    category = forms.CharField(required=False, validators=[validate_category])
-
     class Meta:
         model = RestaurantLocation
         fields = [
@@ -28,18 +24,13 @@ class RestaurantLocationCreateForm(forms.ModelForm):
             'slug',
         ]
 
-# examples!!!
     def clean_name(self):
         name = self.cleaned_data.get("name")
         if name == "Hello":
             raise forms.ValidationError("Not a valid name")
         return name
 
-    # def clean_email(self):
-    #     email = self.cleaned_data.get("email")
-    #     if "edu" in email:
-    #         raise forms.ValidationError("We do not Accept Edu Emails!!!")
-    #     return email
+
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item

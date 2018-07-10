01c127d3bffbc984de54e0cb28092cf8a70a7451
parhamzm <p.zilouchian@gmail.com>
2018-07-10 23:36:14 +0430

    Add some more files!!!

--- BackEnd/tours/forms.py
+++ BackEnd/tours/forms.py
@@ -1,3 +1,11 @@
 from .models import TourVariation
 from django import forms
 
+class TourVariationInventoryForm(forms.ModelForm):
+    class Meta:
+        model = TourVariation
+        fields = [
+            "price",
+            "sale_price",
+            "inventory"
+        ]
--- BackEnd/tours/views.py
+++ BackEnd/tours/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render,get_object_or_404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.shortcuts import render
+from django.http import Http404
 from django.db.models import Q
+from django.utils import timezone
+import datetime
 # Create your views here.
 # class
-
+from .forms import TourVariationInventoryForm
 from .models import Tour, TourVariation
 from attractions.models import Gallery
-import datetime
-from django.utils import timezone
 
 
 class TourVariationListView(ListView):
@@ -18,20 +19,19 @@ class TourVariationListView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(TourVariationListView, self).get_context_data(*args, **kwargs)
-        context["now"] = timezone.now()
-        context["query"] = self.request.GET.get("q")
+
         return context
     # template_name = "tour_list.html"
 
     def get_queryset(self, *args, **kwargs):
-        qs = super(TourVariationListView, self).get_queryset(*args, **kwargs)
-        query = self.request.GET.get("q")
-        # if query:
-        #     qs = self.model.objects.filter(
-        #         Q(name__icontains=query) |
-        #         Q(description__icontains=query)
-        #     )
-        return qs
+        tour_pk = self.kwargs.get("pk")
+        if tour_pk:
+            tour = get_object_or_404(Tour, pk=tour_pk)
+            queryset = TourVariation.objects.filter(tour=tour)
+        return queryset
+
+    def post(self, request, *args, **kwargs):
+        raise Http404
 
 
 class TourListView(ListView):

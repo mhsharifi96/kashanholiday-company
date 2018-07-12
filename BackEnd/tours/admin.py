8c27034a3b37380f2d3794a246bef87065900984
parhamzm <p.zilouchian@gmail.com>
2018-07-12 18:31:16 +0430

    Update requirnments.txt file!!!

--- BackEnd/tours/admin.py
+++ BackEnd/tours/admin.py
@@ -3,6 +3,31 @@ from .models import Tour, TourVariation, TourImage, TourCategory, TourFeatured
 from restaurants.models import RestaurantLocation
 # Register your models here.
 # admin.site.register(Tour)
+
+
+class TourImageInline(admin.TabularInline):
+    model = TourImage
+    extra = 1
+    # max_num =
+
+
+class TourVariationInline(admin.TabularInline):
+    model = TourVariation
+    extra = 1
+
+
+class TourAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "price"]
+
+    inlines = [
+        TourImageInline,
+        TourVariationInline
+    ]
+
+    class Meta:
+        model = Tour
+
+
 admin.site.register(TourVariation)
 admin.site.register(TourImage)
 admin.site.register(TourCategory)
@@ -10,12 +35,15 @@ admin.site.register(TourFeatured)
 # class ProductImagesInline(admin.TabularInline):
 #     model = Tour
 
-class Product2ImagesInline(admin.TabularInline):
-    model = RestaurantLocation
 
-class ProductsAdmin(admin.ModelAdmin):
-    inlines = [
-        Product2ImagesInline
-    ]
+# class Product2ImagesInline(admin.TabularInline):
+#     model = RestaurantLocation
 
-admin.site.register(Tour, ProductsAdmin)
+
+# class ProductsAdmin(admin.ModelAdmin):
+#     inlines = [
+#         Product2ImagesInline
+#     ]
+
+
+admin.site.register(Tour, TourAdmin)

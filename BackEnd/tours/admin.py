97a9aebb98154ddf6c0fd5bd6d8106c96d827523
parhamzm <p.zilouchian@gmail.com>
2018-07-12 09:40:15 +0430

    Add some functionality to the models!!!

--- BackEnd/tours/admin.py
+++ BackEnd/tours/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Tour, TourVariation, TourImage, Category
+from .models import Tour, TourVariation, TourImage, TourCategory
 from restaurants.models import RestaurantLocation
 # Register your models here.
 # admin.site.register(Tour)
 admin.site.register(TourVariation)
 admin.site.register(TourImage)
-admin.site.register(Category)
+admin.site.register(TourCategory)
 # class ProductImagesInline(admin.TabularInline):
 #     model = Tour
 
